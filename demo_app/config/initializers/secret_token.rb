228825e8794c1f45b0b7cc22b7ab3679f4f9b52b
H.Hasebe <hasebe@k-cb.co.jp>
2015-03-02 18:21:43 +0900

    rails _4.0.5_ new demo_app --skip-bundle 実行
    - demo_app/config/database.yml　→ demo_app/config/database.yml.sampleにリネーム

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'ce25859e78b3032f32d08a87d8642cc798f973b60b84a364767b2c61572a5688f7c79ec358bee6fdee0b2f560ebb94aee82892138cb88a3aed8e6d6cf46d5eed'
